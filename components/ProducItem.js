85a743960e03984a8dad5c901bb0e840d6430db8
Octa23 <octa233@gmail.com>
2022-01-05 18:44:50 -0300

    commit cambios

--- components/ProducItem.js
+++ components/ProducItem.js
@@ -32,37 +32,31 @@ const ProductItem = ({item}) => {
         sethover(false);
       }}
     >
-      {hover &&
-        item.cost <= user.points &&(
-            <Box
-              backgroundColor="rgba(21,219,255, .8)"
-              h={"100%"}
-              position={"absolute"}
-              width={"100%"}
+      {hover && item.cost <= user.points && (
+        <Box backgroundColor="rgba(21,219,255, .8)" h={"100%"} position={"absolute"} width={"100%"}>
+          <Stack alignItems={"center"} position={"relative"} top={"30%"}>
+            <Stack direction={"row"} fontSize={"4xl"}>
+              <Text color={"white"}>{item.cost}</Text>
+              <Image
+                alignSelf={"end"}
+                alt="Coin"
+                height={"45px"}
+                src={"/coin.svg"}
+                width={"40px"}
+              />
+            </Stack>
+            <Button
+              borderRadius={99}
+              fontSize={"xl"}
+              height={"50px"}
+              width={"85%"}
+              onClick={handleredeem}
             >
-              <Stack alignItems={"center"} position={"relative"} top={"30%"}>
-                <Stack direction={"row"} fontSize={"4xl"}>
-                  <Text color={"white"}>{item.cost}</Text>
-                  <Image
-                    alignSelf={"end"}
-                    alt="Coin"
-                    height={"45px"}
-                    src={"/coin.svg"}
-                    width={"40px"}
-                  />
-                </Stack>
-                <Button
-                  borderRadius={99}
-                  fontSize={"xl"}
-                  height={"50px"}
-                  width={"85%"}
-                  onClick={handleredeem}
-                >
-                  Redeem now
-                </Button>
-              </Stack>
-            </Box>,
-          )}
+              Redeem now
+            </Button>
+          </Stack>
+        </Box>
+      )}
       <Image margin={"auto"} p={2} src={item.img.url} width={"100%"} />
       {user && item.cost <= user.points && !hover ? (
         <Image
@@ -99,7 +93,7 @@ const ProductItem = ({item}) => {
         </Stack>
       )}
       <Stack pb={5} px={7}>
-        {!hover || item.cost >= user.points ? <Divider /> :  <Divider borderColor={"transparent"}/>}
+        {!hover || item.cost >= user.points ? <Divider /> : null}
         <Stack spacing={0}>
           <Text color={"blackAlpha.600"}>{item.category}</Text>
           <Text>{item.name}</Text>
